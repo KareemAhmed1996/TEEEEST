fa70fb7ad0b58ffd3dedbd2e5e84ad49a246558d
KareemAhmed1996 <58856547+KareemAhmed1996@users.noreply.github.com>
2020-05-16 19:25:23 +0200

    Add files via upload

---
+++ CameraColliders.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraColliders : MonoBehaviour
+{
+    ///Minimum Distance is 1
+    public float minDistance = 1.0f;
+    ///Maximum Distance is 3
+    public float maxDistance = 3.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdjusted;
+    public float distance;
+
+
+    
+    private void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    void Update()
+    {
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.87f), minDistance, maxDistance);
+        }
+        
+        else
+        {
+            distance = maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+
+        }
+
+    /// Following function is used to test
+
+    void TestFunction()
+    {
+        Debug.Log("Test");
+    }
+                
+    }
+
+
+
